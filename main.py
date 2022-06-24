9edb9ed907638750ebc4d586dcfb263ad50a4d32
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-06-24 13:38:51 -0600

    validations_body

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 # Models
+class Location(BaseModel):
+	city: str
+	state: str
+	country: str
+
 class Person(BaseModel):
 	first_name: str
 	last_name: str
@@ -61,6 +66,26 @@ def show_person(
 		..., 
 		gt=0,
 		title="Person ID",
+		description="The person ID"
 	)
 ):
 	return {person_id: "It exists!"}
+
+# Validations: request body
+
+@app.put("/person/{person_id}")
+def update_person(
+	person_id: int = Path(
+		...,
+		title="Person ID",
+		description="This is the person ID",
+		gt=0
+	),
+	person: Person = Body(...),
+	location: Location = Body(...)
+):
+	# Unimos los dos json en uno solo
+	results = person.dict()
+	results.update(location.dict())
+
+	return results
