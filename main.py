16c4b6425413738940457281ea9bb2f8ccc76567
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-06-25 14:13:40 -0600

    reponse_model

--- main.py
+++ main.py
@@ -88,6 +88,10 @@ class Person(BaseModel):
 		default=None,
 		example=False
 	)
+	password: str = Field(
+		...,
+		min_length=8,
+	)
 
 	# class Config:
 	# 	""" 
@@ -104,6 +108,34 @@ class Person(BaseModel):
 	# 		}
 	# 	}
 
+class PersonOut(BaseModel):
+	first_name: str = Field(
+		..., 
+		min_length=1,
+		max_length=50,
+		example="Juan"
+	)
+	last_name: str = Field(
+		..., 
+		min_length=1,
+		max_length=50,
+		example="Lopez"
+	)
+	age: int= Field(
+		..., 
+		gt=0,
+		le=115,
+		example=25
+	)
+	hair_color: Optional[HairColor] = Field(
+		default=None,
+		example="black"
+	)
+	is_married: Optional[bool] = Field(
+		default=None,
+		example=False
+	)
+
 
 # Path operation decoration
 @app.get("/")
@@ -112,7 +144,7 @@ def home():
 
 # Request and response body
 
-@app.post("/person/new")
+@app.post("/person/new", response_model=PersonOut)
 def create_person(person: Person = Body(...)):
 	return person
 
